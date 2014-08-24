5b97a92188b50c195dc052593cc9b5d5d4b8d350
Jesse Gumm <gumm@sigma-star.com>
2014-08-24 04:33:16 -0500

    Update indentation

--- src/element_time_selector.erl
+++ src/element_time_selector.erl
@@ -1,3 +1,4 @@
+%% vim: ts=4 sw=4 et
 -module(element_time_selector).
 -include_lib("nitrogen_core/include/wf.hrl").
 -include("records.hrl").
@@ -6,33 +7,33 @@
 reflect() -> record_info(fields, time_selector).
 
 render_element(Rec = #time_selector{start=Start, finish=Finish, increment=Inc, value=Value, tag=Tag}) ->
-	Times = lists:seq(Start, Finish, Inc),
-	Options = [format_time(T) || T <- Times],
-	ID = wf:temp_id(),
-	Postback = case Tag of
-		undefined -> undefined;
-		_ -> {Tag, ID}
-	end,
-	#dropdown{
-		options=Options,
-		value=Value,
-		id=ID,
-		postback=Postback,
-		delegate=?MODULE,
-		class=Rec#time_selector.class,
-		style=Rec#time_selector.style,
-		data_fields=Rec#time_selector.data_fields
-	}.
+    Times = lists:seq(Start, Finish, Inc),
+    Options = [format_time(T) || T <- Times],
+    ID = wf:temp_id(),
+    Postback = case Tag of
+        undefined -> undefined;
+        _ -> {Tag, ID}
+    end,
+    #dropdown{
+        options=Options,
+        value=Value,
+        id=ID,
+        postback=Postback,
+        delegate=?MODULE,
+        class=Rec#time_selector.class,
+        style=Rec#time_selector.style,
+        data_fields=Rec#time_selector.data_fields
+    }.
 
 event({Tag, ID}) ->
-	Time = wf:to_integer(wf:q(ID)), 
- 	FormattedTime = format_time(Time),
-	Page = wf:page_module(),
-	Page:time_selector_event(Tag, FormattedTime).  
+    Time = wf:to_integer(wf:q(ID)), 
+    FormattedTime = format_time(Time),
+    Page = wf:page_module(),
+    Page:time_selector_event(Tag, FormattedTime).  
 
 
 format_time(Time) ->
-	Hour = Time div 60,
-	Minute = Time rem 60,
-	TimeStr = [wf:to_list(Hour), ":", wf:to_list(Minute)],
-	#option{text=TimeStr, value=wf:to_list(Time)}.
+    Hour = Time div 60,
+    Minute = Time rem 60,
+    TimeStr = [wf:to_list(Hour), ":", wf:to_list(Minute)],
+    #option{text=TimeStr, value=wf:to_list(Time)}.
